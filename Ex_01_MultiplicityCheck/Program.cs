bace2e1c0cfa829ed1c95e47a5869bad1a7855c2
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-02-01 20:32:05 +0300

    Ex_01 added

---
+++ Ex_01_MultiplicityCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+if (number % 7 == 0 && number % 23 == 0)
+{
+  Console.WriteLine($"Число {number} кратно 7 и 23!");
+}
+else
+{
+  Console.WriteLine($"Число {number} НЕ кратно 7 и 23");
+}
